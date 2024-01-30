6350e3475a49f45bc1119b976a205d715b4b3a0d
Areeb Ur Rehman <areebjutt026@gmail.com>
2024-01-31 01:29:40 +0500

    add

---
+++ script.js
@@ -0,0 +1,19 @@
+const quizData = [
+    {
+        question: 'How old is Amanullah?',
+        a: '10',
+        b: '17',
+        c: '24',
+        d: '85',
+        correct: 'c' 
+    }, {
+        question: 'What is the best used programming language in 2023?',
+        a: 'Javascript',
+        b: 'Python',
+        c: 'C',
+        d: 'Java',
+        correct: 'a'
+    }, {
+        question: ''
+    }
+]
